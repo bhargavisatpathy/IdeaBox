8d7acfb25515bca4888e73662a0b308dfe55df09
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2014-12-18 09:04:49 -0700

    idea_box

---
+++ lib/app.rb
@@ -0,0 +1,47 @@
+require 'idea_box'
+
+class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+  set :root, 'lib/app'
+  set :bind, '0.0.0.0'
+  not_found do
+    erb :error
+  end
+
+  get '/' do
+    search_phrase = params[:search_phrase]
+    ideas = search_phrase ? IdeaStore.search(search_phrase) : IdeaStore.all
+    erb :index, locals: {ideas: ideas.sort, idea: Idea.new(params), search_phrase: search_phrase}
+  end
+
+  post '/' do
+    #params.inspect
+    IdeaStore.create(params[:idea])
+    redirect '/'
+  end
+
+  delete '/:id' do |id|
+    IdeaStore.delete(id.to_i)
+    redirect '/'
+    "DELETING an idea!"
+  end
+
+  get '/:id/edit' do |id|
+    idea = IdeaStore.find(id.to_i)
+    erb :edit, locals: {idea: idea}
+  end
+
+  put '/:id' do |id|
+    IdeaStore.update(id.to_i, params[:idea])
+    redirect '/'
+  end
+
+  post '/:id/like' do |id|
+    idea = IdeaStore.find(id.to_i)
+    idea.like!
+    IdeaStore.update(id.to_i, idea.to_h)
+    redirect '/'
+    "I like this idea"
+  end
+
+end
